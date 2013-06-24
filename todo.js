29535966d99d68adb723414f2eed14047181e708
Foo <dotsony@triplehelix.org>
2013-06-24 10:27:10 -0700

    refactor using MVC pattern

--- todo.js
+++ todo.js
@@ -20,13 +20,31 @@ function t(text) {
     return document.createTextNode(text);
 }
 
+function request(method, uri, closure) {
+    var req = new XMLHttpRequest();
 
-function editableList() {
+    req.onreadystatechange = function () {
+	if (req.readyState == 4) {
+	    closure(req.responseText);
+	}
+    };
+
+    req.open(method, uri, true,
+	     login.value,
+	     password.value);
+
+    req.send();
+}
+
+
+function editableList(model) {
     var ret = el("div");
+    var items = [];
     var selected;
 
     function listItem() {
-	var item = el("div", t("Todo Item"));
+	var item = el("div");
+	var content;
 
 	item.contentEditable = "true";
 	item.className = "listItem";
@@ -34,7 +52,7 @@ function editableList() {
 	item.onkeypress = function (evt) {
 	    switch (evt.keyCode) {
 	    case 13:
-		ret.add();
+		model.insert(items.indexOf(item) + 1);
 		evt.preventDefault();
 		break;
 	    };
@@ -42,38 +60,109 @@ function editableList() {
 
 	item.onfocus = function (evt) {
 	    selected = item;
+	    content = item.innerHTML;
 	    setTimeout(
 		function () {
 		    document.getSelection().selectAllChildren(item);
 		}, 1);
 	};
 
+	item.onblur = function (evt) {
+	    if (item.innerHTML === content) {
+		return;
+	    }
+	    item.contentEditable = false;
+	    model.update (items.indexOf(item), item.innerHTML);
+	};
+
 	return item;
     };
 
-    ret.add = function () {
+    model.itemAdded = function (index, content) {
 	var item = listItem();
-	if (selected) {
-	    ret.insertBefore(item, selected.nextSibling);
-	} else {
-	    ret.appendChild(item);
-	}
+	item.innerHTML = content;
+	ret.insertBefore(item, items[index]);
+	items.splice(index, 0, item);
 	item.focus();
     };
 
-    ret.remove = function () {
-	var next = selected ?
-	    (selected.nextSibling || selected.previousSibling) :
-	    null;
+    model.itemChanged = function (index, content) {
+	var item = items[index];
+	item.innerHTML = content;
+	item.contentEditable = true;
+    };
 
-	if (selected) {
-	    ret.removeChild(selected);
-	    next && next.focus();
+    model.itemRemoved = function (index) {
+	var item = items[index];
+	items.splice(index, 1);
+	if (item === selected) {
+	    ((item.nextSibling)
+	     || (item.previousSibling)
+	     || el("unused")).focus();
+	}
+	ret.removeChild(item);
+    };
+
+    ret.append = function () {
+	model.insert(items.indexOf(selected) + 1);
+    };
+
+    ret.remove = function () {
+	if (items.length) {
+	    model.remove(items.indexOf(selected));
 	}
     };
 
     return ret;
 }
+
+
+function listModel(defaultItem) {
+    var items = [];
+    var ret = {};
+
+    ret.itemChanged = function () {};
+    ret.itemAdded = function () {};
+    ret.itemRemoved = function () {};
+
+    ret.forEach = function (closure) {
+	items.forEach(closure);
+    };
+
+    ret.getItem = function (i) {
+	return items[i];
+    };
+
+    ret.getLength = function () {
+	return items.length;
+    };
+
+    ret.update = function (index, item) {
+	items[index] = item || defaultItem;
+	// simulate network roundtrip
+	setTimeout(function () {
+		       ret.itemChanged(index, item);
+		   }, 3000);
+    };
+
+    ret.insert = function(index, item) {
+	var content = item || defaultItem;
+	items.splice(index, 0, content);
+	ret.itemAdded(index, content);
+    };
+
+    ret.append = function(item) {
+	ret.insert(items.length, item);
+    };
+
+    ret.remove = function (index) {
+	items.splice(index, 1);
+	ret.itemRemoved (index);
+    };
+
+    return ret;
+}
+
 
 function isTouchDevice(){
     try {
@@ -109,10 +198,13 @@ var newBtn = get("new");
 var loginBtn = get("doLogin");
 var loginView = get("loginView");
 var listView = get("listView");
-var list = editableList();
+var managerView = get("managerView");
+var lm = listModel("New Item");
+var list = editableList(lm);
+var login = get("login");
+var password = get("password");
 
 get("list").appendChild(list);
-list.add();
 mobileScrollFix(get("screen"));
 
 loginBtn.onclick = function () {
